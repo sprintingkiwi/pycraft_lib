db18aa8d4ff76c97a87fa778a0be68fa086026a5
Alessandro Norfo <sprintingkiwi@gmail.com>
2017-09-18 18:33:52 +0200

    version 0.4

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='pycraft_minetest',
     packages=['pycraft_minetest'],
-    version='0.3',
+    version='0.4',
     description='Modified, simplified and improved libraries to code in Python via Minetest.',
     author='alenorfo & gmenegoz',
     author_email='ale.norfo@gmail.com',
     url='https://github.com/sprintingkiwi/pycraft_lib',
-    download_url='https://github.com/sprintingkiwi/pycraft_lib/tarball/0.3',
+    download_url='https://github.com/sprintingkiwi/pycraft_lib/tarball/0.4',
     keywords=['game', 'development', 'learning', 'education', 'turtle'],
     classifiers=[],
 )
